0199d7c527eec9a1e127e9c32e573c559c7f55b3
Hyadawild <kudosinichi9290@gmail.com>
2025-06-02 18:51:52 +0700

    add news controller

---
+++ src/const/n_data.js
@@ -0,0 +1,24 @@
+
+export const news = [
+  {
+    TItle: "Preorder",
+    Subtl: "Lest go pre order now",
+    image: "/assets/characters/news.jpg",
+    description:
+      "A wandering swordsman who channels the power of storms through his blade.",
+  },
+  {
+    TItle: "Preorder",
+    Subtl: "Lest go pre order now",
+    image: "/assets/characters/news.jpg",
+    description:
+      "A wandering swordsman who channels the power of storms through his blade.",
+  },
+  {
+    TItle: "Preorder",
+    Subtl: "Lest go pre order now",
+    image: "/assets/characters/news.jpg",
+    description:
+      "A wandering swordsman who channels the power of storms through his blade.",
+  },
+]
